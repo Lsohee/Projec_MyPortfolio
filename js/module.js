548607651c5a22173d7aaa668eb2407382d429c9
limsohee <limsohee0521@gmail.com>
2022-05-24 16:44:29 +0900

    물체 생성

---
+++ js/module.js
@@ -0,0 +1,4 @@
+// import { movingObj } from "./module/moving";
+// console.log(movingObj);
+// ? js 허브 파일을 만드는 방법을 모르겠음
+// ? 계속 따라했는데 안됨
---
+++ js/module/moving.js
@@ -0,0 +1,3 @@
+const movingObj = document.getElementById("container");
+console.log(movingObj);
+// ? 왜 콘솔 찍었는데 movingObj가 null로 나올까나
