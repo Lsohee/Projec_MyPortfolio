83d1fff90c1999ccd5950aa2a5b375a8c6882089
limsohee <limsohee0521@gmail.com>
2022-05-25 14:22:43 +0900

    five objs (I,MY,Me,Mine,Myself) styling & javascript calling

--- js/module.js
+++ js/module.js
@@ -1,5 +1,19 @@
-import { root } from "./module/moving";
+import { 
+  movingObj,
+  root,
+  arr
+} from "./module/moving.js";
+console.log(movingObj)
+console.dir(arr);
 console.log(root);
+// console.log(movingObj.movingObjI)
+// 다 가져와지나 확인
+// console.log("root");
+// movingObj.movement();
+// 일단 외부 script에서 함수를 어떻게 호출하는지 확인
+
+
+
 // ? js 허브 파일을 만드는 방법을 모르겠음
 // ? 계속 따라했는데 안됨
 // memo script 태그에 type = "module" 쓰기로 해결
--- js/module/moving.js
+++ js/module/moving.js
@@ -1,5 +1,49 @@
 export const root = document.getElementById("container");
-console.log(root);
-const movingObjI = document.querySelector("#container>div:nth-child(1)")
-console.log(movingObjI);
-// ? 왜 콘솔 찍었는데 movingObj가 null로 나올까나
+export const arr = Array.from(container.children);
+export const movingObj = {
+
+movingObjI : arr[0],
+movingObjMy : arr[1],
+movingObjMe : arr[2],
+movingObjMine : arr[3],
+movingObjMyself : arr[4],
+
+/*
+    이벤트 발생시 변화를 줘야할 요소
+    1. 물체들의 크기
+    2. 블러 처리
+    3. 움직임
+*/ 
+//   zoomIn : function(obj){
+//   let widthValue ;
+//   let heightValue;
+//   // widthValue ;
+//   // heightValue;
+//   console.dir(obj)
+//   obj.style.height = heightValue + "px";
+//   obj.style.width = widthValue + "px";
+
+//   if(widthValue ===300){
+//     console.log("꿈이 아파 잠들지 못하는 밤")
+//     // heightValue = 400;
+//     // widthValue = 400;
+//     // 똑딱이 잘 됨 -> 스위치 완료
+//     // currnt =false;
+//     return false;
+//   }else {
+//     console.log("작은 숨소리마저 아려와")
+//     widthValue = 300;
+//     heightValue = 300;
+//     // currnt =true;
+//   }
+//   // return move;
+// },
+
+
+
+// movement : root.addEventListener("click", this.zoomIn(movingObj.movingObjMine))
+
+}
+// console.log(movingObj.movingObjI);
+// console.log(movingObjI);
+// // ? 왜 콘솔 찍었는데 movingObj가 null로 나올까나
