6e7ffc2f7d96d5700c6a991d68fdae09ebb3bcdb
limsohee <limsohee0521@gmail.com>
2022-05-25 11:44:35 +0900

    물체 5개 (I,MY,Me,Mine,Myself)생성

--- js/module.js
+++ js/module.js
@@ -1,4 +1,5 @@
-// import { movingObj } from "./module/moving";
-// console.log(movingObj);
+import { root } from "./module/moving";
+console.log(root);
 // ? js 허브 파일을 만드는 방법을 모르겠음
 // ? 계속 따라했는데 안됨
+// memo script 태그에 type = "module" 쓰기로 해결
--- js/module/moving.js
+++ js/module/moving.js
@@ -1,3 +1,5 @@
-const movingObj = document.getElementById("container");
-console.log(movingObj);
+export const root = document.getElementById("container");
+console.log(root);
+const movingObjI = document.querySelector("#container>div:nth-child(1)")
+console.log(movingObjI);
 // ? 왜 콘솔 찍었는데 movingObj가 null로 나올까나
